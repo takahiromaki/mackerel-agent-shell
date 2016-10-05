0781955de259a6f3bffcfcd21fa5cfa444bda459
Takahiro Maki <t-maki@glad-cube.com>
2016-10-05 11:31:29 +0900

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+if [$# -ne 1]; then
+  echo "Not found mackerel API Key"
+  exit 1
+fi
+set API_KEY = $1
+
+# download
+cd ~
+curl -O https://mackerel.io/file/agent/tgz/mackerel-agent-latest.tar.gz
+tar xfvz ./mackerel-agent-latest.tar.gz
+mv ./mackerel-agent /etc/
+
+# write service file
+cat <<EOF > /etc/systemd/system/mackerel-agent.service
+[Unit]
+Description = mackerel-agent
+
+[Service]
+ExecStartPre = /etc/mackerel-agent/mackerel-agent init -apikey=$API_KEY
+ExecStart = /etc/mackerel-agent/mackerel-agent --conf=/etc/mackerel-agent/mackerel-agent.conf
+Type = simple
+User = root
+Group = root
+Restart = always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# enable auto restart
+systemctl enable mackerel-agent.service
+
+# start service
+systemctl start mackerel-agent.service
+
+# check service status
+echo "[Mackerel service status]"
+systemctl status mackerel-agent.service | cat
+
+echo "Complete!"
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Description
+Install mackerel agent to CoreOS.
+
+## Environments
+Linux with systemd can be used.
+
+## How to use
+API_KEY is mackerel API Key
+```
+$ git clone https://github.com/takahirio_maki/mackerel-agent-shell.git
+$ sudo su
+$ sh ./mackerel-agent-shell/install.sh API_KEY
+```
